fbaa029d0a70462996d035ee1a22cd26d459979c
g0urav_hustler <gouravchouhan966@gmail.com>
2021-12-10 18:07:49 +0530

    creating home page

--- App/lib/home_page.dart
+++ App/lib/home_page.dart
@@ -3,6 +3,33 @@ import "package:flutter/material.dart";
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        body: Container(
+      height: MediaQuery.of(context).size.height,
+      width: MediaQuery.of(context).size.width,
+      decoration: BoxDecoration(color: Colors.green),
+      child: Row(
+        children: [
+          Container(
+              height: MediaQuery.of(context).size.height / 3,
+              child: Text(
+                "Electronic Component Scanner",
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                ),
+              )),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+                decoration: BoxDecoration(color: Colors.white),
+                child: Column(
+                  children: [],
+                )),
+          )
+        ],
+      ),
+    ));
   }
 }
