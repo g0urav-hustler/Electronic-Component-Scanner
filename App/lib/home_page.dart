ff47bc851dbdd689a2773ca3a8a8a80df9377131
g0urav_hustler <gouravchouhan966@gmail.com>
2021-12-11 21:07:19 +0530

    update homepage

--- App/lib/home_page.dart
+++ App/lib/home_page.dart
@@ -4,32 +4,42 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-      height: MediaQuery.of(context).size.height,
-      width: MediaQuery.of(context).size.width,
-      decoration: BoxDecoration(color: Colors.green),
-      child: Row(
-        children: [
-          Container(
-              height: MediaQuery.of(context).size.height / 3,
-              child: Text(
-                "Electronic Component Scanner",
-                textAlign: TextAlign.left,
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
-                ),
-              )),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-                decoration: BoxDecoration(color: Colors.white),
-                child: Column(
-                  children: [],
-                )),
-          )
-        ],
+      appBar: AppBar(
+        title: Text("Awesome App"),
       ),
-    ));
+      body: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: SingleChildScrollView(
+            child: Card(
+                child: Column(children: [
+              Image.asset('images/file1131.jpg', fit: BoxFit.cover, width: 350, height: 200),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                myText,
+                style: TextStyle(
+                  fontSize: 25,
+                  color: Colors.blueAccent[400],
+                ),
+              ),
+              SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                    controller: _nameController,
+                    // obscureText: true, for password **
+                    decoration: InputDecoration(hintText: "Enter something here", labelText: "Name", border: OutlineInputBorder())),
+              )
+            ])),
+          )),
+      drawer: MyDrawer(),
+      floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            myText = _nameController.text;
+            setState(() {});
+          },
+          child: Icon(Icons.refresh)),
+    );
   }
 }
