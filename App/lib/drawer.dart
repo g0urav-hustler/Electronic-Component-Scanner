288fafa9e87bfb1f5fe1e21e68171e25128bc602
g0urav_hustler <gouravchouhan966@gmail.com>
2021-12-13 23:13:26 +0530

    add drawer

---
+++ App/lib/drawer.dart
@@ -0,0 +1,268 @@
+import 'package:flutter/material.dart';
+
+
+class MyappdrawerWidget extends StatelessWidget {
+          @override
+          Widget build(BuildContext context) {
+          // Figma Flutter Generator MyappdrawerWidget - FRAME
+            return Container(
+      width: 212,
+      height: 285,
+      decoration: BoxDecoration(
+          borderRadius : BorderRadius.only(
+            topLeft: Radius.circular(20),
+            topRight: Radius.circular(20),
+            bottomLeft: Radius.circular(20),
+            bottomRight: Radius.circular(20),
+          ),
+      color : Color.fromRGBO(255, 255, 255, 1),
+  ),
+      child: Stack(
+        children: <Widget>[
+          Positioned(
+        top: 30,
+        left: 10,
+        child: Container(
+        width: 193,
+        height: 36,
+        decoration: BoxDecoration(
+          borderRadius : BorderRadius.only(
+            topLeft: Radius.circular(20),
+            topRight: Radius.circular(20),
+            bottomLeft: Radius.circular(20),
+            bottomRight: Radius.circular(20),
+          ),
+      color : Color.fromRGBO(80, 200, 120, 0.8799999952316284),
+  )
+      )
+      ),Positioned(
+        top: 88,
+        left: 10,
+        child: Container(
+        width: 193,
+        height: 36,
+        decoration: BoxDecoration(
+          borderRadius : BorderRadius.only(
+            topLeft: Radius.circular(20),
+            topRight: Radius.circular(20),
+            bottomLeft: Radius.circular(20),
+            bottomRight: Radius.circular(20),
+          ),
+      color : Color.fromRGBO(80, 200, 120, 0.8799999952316284),
+  )
+      )
+      ),Positioned(
+        top: 88,
+        left: 10,
+        child: Container(
+        width: 193,
+        height: 36,
+        decoration: BoxDecoration(
+          borderRadius : BorderRadius.only(
+            topLeft: Radius.circular(20),
+            topRight: Radius.circular(20),
+            bottomLeft: Radius.circular(20),
+            bottomRight: Radius.circular(20),
+          ),
+      color : Color.fromRGBO(80, 200, 120, 0.8799999952316284),
+  )
+      )
+      ),Positioned(
+        top: 146,
+        left: 10,
+        child: Container(
+        width: 193,
+        height: 36,
+        decoration: BoxDecoration(
+          borderRadius : BorderRadius.only(
+            topLeft: Radius.circular(20),
+            topRight: Radius.circular(20),
+            bottomLeft: Radius.circular(20),
+            bottomRight: Radius.circular(20),
+          ),
+      color : Color.fromRGBO(80, 200, 120, 0.8799999952316284),
+  )
+      )
+      ),Positioned(
+        top: 204,
+        left: 10,
+        child: Container(
+        width: 193,
+        height: 36,
+        decoration: BoxDecoration(
+          borderRadius : BorderRadius.only(
+            topLeft: Radius.circular(20),
+            topRight: Radius.circular(20),
+            bottomLeft: Radius.circular(20),
+            bottomRight: Radius.circular(20),
+          ),
+      color : Color.fromRGBO(80, 200, 120, 0.8799999952316284),
+  )
+      )
+      ),Positioned(
+        top: 39,
+        left: 56,
+        child: Text('About App', textAlign: TextAlign.left, style: TextStyle(
+        color: Color.fromRGBO(255, 255, 255, 0.9700000286102295),
+        fontFamily: 'Roboto Slab',
+        fontSize: 15,
+        letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
+        fontWeight: FontWeight.normal,
+        height: 1
+      ),)
+      ),Positioned(
+        top: 99,
+        left: 49,
+        child: Text('Share App', textAlign: TextAlign.left, style: TextStyle(
+        color: Color.fromRGBO(255, 255, 255, 0.9700000286102295),
+        fontFamily: 'Roboto Slab',
+        fontSize: 15,
+        letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
+        fontWeight: FontWeight.normal,
+        height: 1
+      ),)
+      ),Positioned(
+        top: 156,
+        left: 52,
+        child: Text('Rate Us', textAlign: TextAlign.left, style: TextStyle(
+        color: Color.fromRGBO(255, 255, 255, 0.9700000286102295),
+        fontFamily: 'Roboto Slab',
+        fontSize: 15,
+        letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
+        fontWeight: FontWeight.normal,
+        height: 1
+      ),)
+      ),Positioned(
+        top: 214,
+        left: 49,
+        child: Text('About Contributor', textAlign: TextAlign.left, style: TextStyle(
+        color: Color.fromRGBO(255, 255, 255, 0.9700000286102295),
+        fontFamily: 'Roboto Slab',
+        fontSize: 15,
+        letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
+        fontWeight: FontWeight.normal,
+        height: 1
+      ),)
+      ),Positioned(
+        top: 35,
+        left: 25,
+        child: Container(
+      width: 24,
+      height: 24,
+      decoration: BoxDecoration(
+          color : Color.fromRGBO(255, 255, 255, 0),
+  ),
+      child: Stack(
+        children: <Widget>[
+          Positioned(
+        top: 0,
+        left: 0,
+        child: SvgPicture.asset(
+        'assets/images/vector.svg',
+        semanticsLabel: 'vector'
+      );
+      ),Positioned(
+        top: 2,
+        left: 2,
+        child: SvgPicture.asset(
+        'assets/images/vector.svg',
+        semanticsLabel: 'vector'
+      );
+      ),
+        ]
+      )
+    )
+      ),Positioned(
+        top: 94,
+        left: 23,
+        child: Container(
+      width: 24,
+      height: 24,
+      decoration: BoxDecoration(
+          color : Color.fromRGBO(255, 255, 255, 1),
+  ),
+      child: Stack(
+        children: <Widget>[
+          Positioned(
+        top: 0,
+        left: 0,
+        child: SvgPicture.asset(
+        'assets/images/vector.svg',
+        semanticsLabel: 'vector'
+      );
+      ),Positioned(
+        top: 1.9983315467834473,
+        left: 1.9997941255569458,
+        child: SvgPicture.asset(
+        'assets/images/vector.svg',
+        semanticsLabel: 'vector'
+      );
+      ),
+        ]
+      )
+    )
+      ),Positioned(
+        top: 152,
+        left: 20,
+        child: Container(
+      width: 24,
+      height: 24,
+      decoration: BoxDecoration(
+          color : Color.fromRGBO(255, 255, 255, 1),
+  ),
+      child: Stack(
+        children: <Widget>[
+          Positioned(
+        top: 0,
+        left: 0,
+        child: SvgPicture.asset(
+        'assets/images/vector.svg',
+        semanticsLabel: 'vector'
+      );
+      ),Positioned(
+        top: 2.5,
+        left: 2.490000009536743,
+        child: SvgPicture.asset(
+        'assets/images/vector.svg',
+        semanticsLabel: 'vector'
+      );
+      ),
+        ]
+      )
+    )
+      ),Positioned(
+        top: 210,
+        left: 18,
+        child: Container(
+      width: 24,
+      height: 24,
+      decoration: BoxDecoration(
+          color : Color.fromRGBO(255, 255, 255, 1),
+  ),
+      child: Stack(
+        children: <Widget>[
+          Positioned(
+        top: 0,
+        left: 0,
+        child: SvgPicture.asset(
+        'assets/images/vector.svg',
+        semanticsLabel: 'vector'
+      );
+      ),Positioned(
+        top: 2,
+        left: 2,
+        child: SvgPicture.asset(
+        'assets/images/vector.svg',
+        semanticsLabel: 'vector'
+      );
+      ),
+        ]
+      )
+    )
+      ),
+        ]
+      )
+    );
+          }
+        }
+        
